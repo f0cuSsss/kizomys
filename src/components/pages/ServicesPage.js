53193df72e84da267752699c61a35ece37d4e92a
f0cuS <f0cuSmcsza@gmail.com>
2021-01-11 16:23:44 +0200

    Services page created

--- src/components/pages/ServicesPage.js
+++ src/components/pages/ServicesPage.js
@@ -2,10 +2,34 @@ import React from 'react'
 
 import '../../styles/pages/ServicesPage.css'
 
+const servicesItems = [
+    { service: 'Спуск лодки', price: '50 грн.' },
+    { service: 'Аренда лодки', price: 'от 150 грн./сутки' },
+    { service: 'Аренда мотора', price: 'от 250 грн./сутки' }
+];
+
 const ServicesPage = () => {
     return (
         <div>
-            ServicesPage
+            <div className="e-title">Наши услуги</div>
+            <div className="services-table">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Услуга</th>
+                            <th>Цена</th>
+                        </tr>
+                        {servicesItems.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.service}</td>
+                                    <td>{item.price}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
